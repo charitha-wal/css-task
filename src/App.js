9857b3db2f060e79b31bd564c5b3fe42a50e2913
charitha-wal <sreecharitha.m@westagilelabs.com>
2020-07-17 20:06:55 +0530

    deBounce hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-import useOutsideClick from './component/useOutsideClick';
+// import useOutsideClick from './component/useOutsideClick';
+import useDebounce from './component/useDebounce';
 
 function App() {
 
-  const myRef = useRef();
-  let outsideClicked = useOutsideClick(myRef);
-  console.log(outsideClicked);
+  // const myRef = useRef();
+  // let outsideClicked = useOutsideClick(myRef);
+  // console.log(outsideClicked);
+  // return (
+  //   <div style={{ height: "100vh", width: "100vw", textAlign: "center" }}>
+  //     <div style={{ height: "30vh", width: "30vw", backgroundColor: "#eee", margin: "auto" }} ref={myRef}>
+  //       <p>{outsideClicked ? "Clicked outside this div" : "Clicked on this div"}</p>
+  //     </div>
+  //   </div>
+  // );
+  const [state, setState] = useState(0);
+  const [debounce, setDebounce] = useState(0);
+  const debouncedValue = useDebounce(debounce, 3000);
+
+  useEffect(() => {
+    console.log(state, debounce);
+  }, [debouncedValue]);
+
   return (
-    <div style={{ height: "100vh", width: "100vw", textAlign: "center" }}>
-      <div style={{ height: "30vh", width: "30vw", backgroundColor: "#eee", margin: "auto" }} ref={myRef}>
-        <p>{outsideClicked ? "Clicked outside this div" : "Clicked on this div"}</p>
-      </div>
+    <div style={{ height: "50vh", width: "50vw" }}>
+      <button onClick={() => { setState(state + 1) }} >{state}</button>
+      <button onClick={() => { setDebounce(debouncedValue + 1) }} >{debounce}</button>
     </div>
   );
+
 }
 
 export default App;
